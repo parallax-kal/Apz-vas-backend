ed3b55de14fe91fdb90a13f94bc812170b16add3
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2023-05-19 14:30:01 +0200

    Added protection to VASProvider, VASService Routes

--- routes/VASProvider.go
+++ routes/VASProvider.go
@@ -2,10 +2,15 @@ package routes
 
 import (
 	"apz-vas/controllers"
+	"apz-vas/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func InitializeVASProviderRoutes(router *gin.RouterGroup) {
-	router.POST("/create-vas-provider", controllers.CreateVASProvider())
+	router.POST("/create-vas-provider",
+		middlewares.AuthMiddleware(),
+		middlewares.AdminMiddleware(),
+		controllers.CreateVASProvider(),
+	)
 	// router.GET("/get-vas-providers", controllers.GetVASProviders())
 }
--- routes/VASService.go
+++ routes/VASService.go
@@ -2,11 +2,16 @@ package routes
 
 import (
 	"apz-vas/controllers"
+	"apz-vas/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func InitializeVASServiceRoutes(router *gin.RouterGroup) {
-	router.POST("/create-vas-service", controllers.CreateVasService())
+	router.POST("/create-vas-service",
+		middlewares.AuthMiddleware(),
+		middlewares.AdminMiddleware(),
+		controllers.CreateVasService(),
+	)
 	// router.GET("/get-vas-services", controllers.GetVASServices())
 	// router.GET("/get-vas-service/:id", controllers.GetVASService())
 	// router.PUT("/update-vas-service/:id", controllers.UpdateVASService())
