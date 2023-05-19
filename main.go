6c2237e217fda2cdcabb3428b6d3c6cf966e4d14
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2023-05-19 05:57:59 +0200

    testing

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"apz-vas/configs"
-	"apz-vas/routes"
+	// "apz-vas/configs"
+	// "apz-vas/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-	_, err := configs.ConnectDb()
-	if err != nil {
-		panic(err)
-	}
+	// _, err := configs.ConnectDb()
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	// use the routes
 
-	routes.InitializeRoutes(router)
+	// routes.InitializeRoutes(router)
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, "Welcome to APZ-VAS API")
