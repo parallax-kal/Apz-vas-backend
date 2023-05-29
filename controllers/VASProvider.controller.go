53fd71cbcd2317525395e2534fabc96eec051c38
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2023-05-29 08:14:50 +0200

    some changes in a new pc

--- controllers/VASProvider.controller.go
+++ controllers/VASProvider.controller.go
@@ -1,4 +1,4 @@
-package controllers
+	package controllers
 
 import (
 	"apz-vas/configs"
--- main.go
+++ main.go
@@ -8,13 +8,14 @@ import (
 )
 
 func main() {
-	gin.SetMode(gin.ReleaseMode)
+	// gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
+	
 	_, err := configs.ConnectDb()
 	if err != nil {
 		panic(err)
 	}
-
+	
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE"},
@@ -24,8 +25,9 @@ func main() {
 	}))
 
 	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, "Welcome to APZ VAS")
+		c.String(200, "Welcome to APZ VAS")
 	})
+
 	routes.InitializeRoutes(router)
 	router.Run(":5000")
 }
